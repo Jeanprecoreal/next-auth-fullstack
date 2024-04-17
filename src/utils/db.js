45f61f159df983130af7f6f6118492fd58ec35fc
Jeanprecoreal <jean.precoreal@gmail.com>
2024-04-17 11:27:40 -0400

    aff

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 export default async function connect() {
   try {
     await mongoose.connect(process.env.MONGO);
+    console.log("conectado");
   } catch (error) {
+    console.log("não conectado");
     throw new Error(error);
   }
 }
